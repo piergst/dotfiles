3b3405d694a29e1e30365a923a3d2bd77b0a38d2
nderflow <pierre.giusti@allvei.com>
2026-03-25 21:29:30 +0100

    clean

--- dot_exegol/my-resources/setup/nvim/nvim/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- -- Add any additional options here
--- vim.g.mapleader = "."
-vim.diagnostic.enable(false)
-vim.opt.spelllang = { "fr", "en" }
